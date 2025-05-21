b1a5316f69b6f285ea4ee543ab25b9edf0a19862
maearon <manhng132@gmail.com>
2025-05-21 16:58:17 +0700

    fixx all bug import, dotnet dev-certs https --trust, https://localhost:7214

--- Controllers/Api/AuthController.cs
+++ Controllers/Api/AuthController.cs
@@ -2,6 +2,7 @@ using DotNetBoilerplate.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
--- Controllers/Api/RelationshipsController.cs
+++ Controllers/Api/RelationshipsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 
 namespace DotNetBoilerplate.Controllers.Api
