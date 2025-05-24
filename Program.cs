2f160f6f3746e40456f40e3ab62f935e29658f99
maearon <manhng132@gmail.com>
2025-05-24 09:02:08 +0700

    downgrade dotnet 9 to 8 for all packages

--- Program.cs
+++ Program.cs
@@ -122,7 +122,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("https://vue-dot-net-boilerplate.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
